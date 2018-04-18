f28c7c14082cdcafefbe355c7cd5cda6a316433c
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-04-18 01:45:29 -0300

    Adicionando imagens

--- resources/views/index.php
+++ resources/views/index.php
@@ -31,13 +31,13 @@
 			-->
 			<p>
 				<b>Redes Sociais:</b><br/>
-				<a href="https://goo.gl/729QP" target="_blank"><img src="resources/imagens/github2.png" width="90" height="90"></a>
-				<a href="https://goo.gl/Rx0G1" target="_blank"><img src="resources/imagens/twitter2.png" width="90" height="90"></a>
-				<a href="https://goo.gl/FNd1sV" target="_blank"><img src="resources/imagens/linkedin2.png" width="90" height="90"></a>
-				<a href="https://goo.gl/lwF6Sz" target="_blank"><img src="resources/imagens/slideshare.png" width="90" height="90"></a>
-				<a href="https://goo.gl/aP2Ku3" target="_blank"><img src="resources/imagens/instagram.png" width="90" height="90"></a>
-				<a href="https://goo.gl/766xci" target="_blank"><img src="resources/imagens/facebook.png" width="90" height="90"></a>
-				<a href="https://goo.gl/zAsIGY" target="_blank"><img src="resources/imagens/skoob.png" width="90" height="90"></a>
+				<a href="https://goo.gl/729QP" target="_blank"><img src="images/github.png" width="90" height="90"></a>
+				<a href="https://goo.gl/FNd1sV" target="_blank"><img src="images/linkedin.png" width="90" height="90"></a>
+				<a href="https://goo.gl/Rx0G1" target="_blank"><img src="images/twitter.png" width="90" height="90"></a>
+				<a href="https://goo.gl/lwF6Sz" target="_blank"><img src="images/slideshare.png" width="90" height="90"></a>
+				<a href="https://goo.gl/aP2Ku3" target="_blank"><img src="images/instagram.png" width="90" height="90"></a>
+				<a href="https://goo.gl/766xci" target="_blank"><img src="images/facebook.png" width="90" height="90"></a>
+				<a href="https://goo.gl/zAsIGY" target="_blank"><img src="images/skoob.png" width="90" height="90"></a>
 			</p>
 			<br/><a href="https://about.me/rodrigoamora" target="_blank">About.me</a>
 		</div>
