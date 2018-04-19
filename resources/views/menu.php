25f19f85f661b3b54ad533cc4eba162decc210f8
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-04-19 00:30:19 -0300

    Corrigindo a palavra portfólio

--- resources/views/menu.php
+++ resources/views/menu.php
@@ -2,7 +2,7 @@
 	<h5 class="my-0 mr-md-auto font-weight-normal">Rodrigo Amora</h5>
 	<nav class="my-2 my-md-0 mr-md-3">
 		<a class="p-2 text-dark" href="/">Home</a>
-		<a class="p-2 text-dark" href="/projetos">Portif&oacute;lio</a>
+		<a class="p-2 text-dark" href="/projetos">Portf&oacute;lio</a>
 		<a  class="p-2 text-dark" href="#">Contato</a>
 		<a  class="p-2 text-dark" href="http://goo.gl/FQg16" target="_blanlk">Blog</a>
 	</nav>
