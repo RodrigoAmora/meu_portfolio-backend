c9fffe2df089d12945043750a68e48a068e51441
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-04-20 02:00:25 -0300

    Adicionando o footer no layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,9 @@
 <div class="container">
 
 	<nav class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-		<h5 class="my-0 mr-md-auto font-weight-normal">Rodrigo Amora</h5>
+		<h5 class="my-0 mr-md-auto font-weight-normal">
+			<a class="text-muted" href="/">Rodrigo Amora</a>
+		</h5>
 		<nav class="my-2 my-md-0 mr-md-3">
 			<a class="p-2 text-dark" href="/">Home</a>
 			<a class="p-2 text-dark" href="/projetos">Portf&oacute;lio</a>
@@ -20,8 +22,47 @@
 	@yield('body')
 	@yield('projetos')
 
-	<footer>
-		<h2>Rodrigo Amora</h2>
+	<footer class="pt-4 my-md-5 pt-md-5 border-top">
+		<div class="row">
+			<div class="col-12 col-md">
+				<h5>Contato:</h5><hr>
+				<ul class="list-unstyled text-small">
+					<li class="text-muted">
+						<img class="rounded" src="images/gmail.svg" width="25" height="25">
+						<strong>E-mail:</strong> rodrigo.amora.freitas@gmail.com
+					</li>
+				</ul>
+			</div>
+
+			<div class="col-12 col-md">
+				<div class="card bg-light mb-3" style="max-width: 18rem;">
+					<div class="card-header">Redes Socais:</div>
+					<div class="card-body">
+						<br/>
+						<a href="https://goo.gl/729QP" target="_blank"><img class="rounded" src="images/github.png" width="50" height="50"></a>
+						<a href="https://goo.gl/FNd1sV" target="_blank"><img class="rounded" src="images/linkedin.png" width="50" height="50"></a>
+						<a href="https://goo.gl/Rx0G1" target="_blank"><img class="rounded" src="images/twitter.png" width="50" height="50"></a>
+						<a href="https://goo.gl/lwF6Sz" target="_blank"><img class="rounded" src="images/slideshare.png" width="50" height="50"></a>
+						<a href="https://goo.gl/aP2Ku3" target="_blank"><img class="rounded" src="images/instagram.png" width="50" height="50"></a>
+						<a href="https://goo.gl/766xci" target="_blank"><img class="rounded" src="images/facebook.png" width="50" height="50"></a>
+						<a href="https://goo.gl/zAsIGY" target="_blank"><img class="rounded" src="images/skoob.png" width="50" height="50"></a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-12 col-md">
+				<h5>Menu:</h5><hr>
+				<ul class="list-unstyled text-small">
+					<li class="text-muted">
+						<a class="text-muted" href="/">Home</a>
+					</li>
+
+					<li class="text-muted">
+						<a class="text-muted" href="/projetos">Projetos</a>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</footer>
 </div>
 </body>
