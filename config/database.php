81c7437db04d716900c1d5b964a8b3388d94736d
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-04-22 16:50:16 -0300

    Colocando as informações do banco de dados no arquivo .env

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'mysql.rodrigoamora.com.br'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'rodrigoamora'),
-            'username' => env('DB_USERNAME', 'rodrigoamora'),
-            'password' => env('DB_PASSWORD', 'bat2011'),
+            'host' => env('DB_HOST', env('DB_HOST')),
+            'port' => env('DB_PORT', env('DB_PORT')),
+            'database' => env('DB_DATABASE', env('DB_DATABASE')),
+            'username' => env('DB_USERNAME', env('DB_USERNAME')),
+            'password' => env('DB_PASSWORD', env('DB_PASSWORD')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
