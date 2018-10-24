8bed243bd8839122ab8c466acc8f44e29a499b68
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-10-24 00:22:01 -0200

    Ajustes

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -6,16 +6,15 @@
 	<h1>Admin</h1>
 	@auth
 		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+			<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+				{{ __('Logout') }}
+            </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+		</div>
+		
 	@endauth
 	<hr>
 	
